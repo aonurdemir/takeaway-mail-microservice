9b51b5b017047bb523e1846a162b1942f0114ae7
Onur Demir <aaonurdemir@gmail.com>
2020-12-12 14:20:41 +0300

    Create mail model,seed,factory and migration

---
+++ app/Models/Mail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mail extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mails';
+
+    public const STATE_CREATED    = 'created';
+    public const STATE_PROCESSING = 'processing';
+    public const STATE_SENT       = 'sent';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'from',
+        'to',
+        'subject',
+        'content',
+        'state',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
