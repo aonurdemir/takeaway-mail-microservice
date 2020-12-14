ff501584ab3f7f4ce91870ac4036647648ad05e9
Onur Demir <aaonurdemir@gmail.com>
2020-12-15 00:45:39 +0300

    Add createAllServices method to MailServiceFactory in order to get all instances for all third party email services

--- app/Services/MailServiceFactory.php
+++ app/Services/MailServiceFactory.php
@@ -11,6 +11,11 @@ class MailServiceFactory
     public const SENDGRID = 'sendgrid';
     public const MAILJET  = 'mailjet';
 
+    private const SERVICE_NAMES = [
+        self::SENDGRID,
+        self::MAILJET,
+    ];
+
 
     /**
      * @param string $service
@@ -22,12 +27,27 @@ class MailServiceFactory
     {
         switch ($service) {
             case static::SENDGRID:
-                return new MailServiceSendGridImp();
+                return MailServiceSendGridImp::create();
             case static::MAILJET:
                 return MailServiceMailjetImp::ofVersion('v3.1');
             default:
                 throw new UndefinedMailService($service);
         }
+    }
+
+
+    /**
+     * @return array
+     * @throws \App\Exceptions\UndefinedMailService
+     */
+    public static function createAllServices(): array
+    {
+        $services = [];
+        foreach (self::SERVICE_NAMES as $serviceName) {
+            array_push($services, static::create($serviceName));
+        }
+
+        return $services;
     }
 
 }
