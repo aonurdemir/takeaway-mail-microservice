d169bdec8249b957951ecda359d389d49d016021
Onur Demir <aaonurdemir@gmail.com>
2020-12-17 00:40:26 +0300

    Fix bug of throwing NoAvailableThirdPartyMailService exception although the mail was sent

--- app/Services/MailSender.php
+++ app/Services/MailSender.php
@@ -43,9 +43,9 @@ class MailSender
 
         if (! $this->isMailSent()) {
             $this->setMailJobAsFailed();
-        }
 
-        throw new NoAvailableThirdPartyMailService();
+            throw new NoAvailableThirdPartyMailService();
+        }
     }
 
     private function setMailServiceByPollingFromQueue(): void
