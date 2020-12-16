9728f4da119ae62e00b397ab8cf5653b2736b8c0
Onur Demir <aaonurdemir@gmail.com>
2020-12-17 00:44:02 +0300

    Add EloquentMailJobRepository as the MailJobRepository to the AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\EloquentMailJobRepository;
+use App\Repositories\MailJobRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(MailJobRepository::class, function ($app) {
+            return new EloquentMailJobRepository();
+        });
     }
 
     /**
