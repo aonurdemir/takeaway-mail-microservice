a7c5e0a61f4722fd991aa35c254e702ebbab98d8
Onur Demir <aaonurdemir@gmail.com>
2020-12-18 13:38:04 +0300

    Add CustomerRegistered event into UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Events\CustomerRegistered;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function store(Request $request)
     {
+        $attributes = $request->only(
+            [
+                'email',
+            ]
+        );
+
+        //Assume a user service is creating the new user and dispatches CustomerRegistered event
+        CustomerRegistered::dispatch();
+
         return response()->json(['message' => 'created'], 201);
     }
 }
