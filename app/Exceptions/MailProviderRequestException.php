e68a9849ee7e08e1c0fbb3312509b3fd43ef3a76
Onur Demir <aaonurdemir@gmail.com>
2020-12-18 21:08:03 +0300

    Refactor MailProviderRequestException.php

--- app/Exceptions/MailProviderRequestException.php
+++ app/Exceptions/MailProviderRequestException.php
@@ -8,24 +8,8 @@ use Exception;
 
 class MailProviderRequestException extends Exception
 {
-    private string $providerName;
-    private int    $statusCode;
-
     public function __construct(string $providerName, int $statusCode)
     {
-        parent::__construct();
-
-        $this->providerName = $providerName;
-        $this->statusCode = $statusCode;
-    }
-
-    public function getProviderName(): string
-    {
-        return $this->providerName;
-    }
-
-    public function getStatusCode(): int
-    {
-        return $this->statusCode;
+        parent::__construct("Provider {$providerName} returned status code {$statusCode}");
     }
 }
