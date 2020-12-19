70f51f003c25bddf5ea99b2bec20b11dde79eb4d
Onur Demir <aaonurdemir@gmail.com>
2020-12-19 15:32:54 +0300

    Fix MailProviderIteratorTest.php

--- tests/Unit/Services/Utils/MailProviderIteratorTest.php
+++ tests/Unit/Services/Utils/MailProviderIteratorTest.php
@@ -3,62 +3,68 @@
 namespace Tests\Unit\Services\Utils;
 
 use App\Exceptions\NoSuchProviderException;
-use App\Factories\MailProviderFactory;
+use App\Services\SendGridMailProvider;
 use App\Services\Utils\MailProviderIterator;
+use Mockery\MockInterface;
 use Tests\TestCase;
 
 class MailProviderIteratorTest extends TestCase
 {
     public function test_empty_iterator_has_next()
     {
-        $iterator = new MailProviderIterator([]);
-
+        $iterator = new MailProviderIterator();
         $this->assertFalse($iterator->hasNext());
     }
 
     public function test_next_of_empty_iterator()
     {
-        $iterator = new MailProviderIterator([]);
+        $iterator = new MailProviderIterator();
 
         $this->expectException(NoSuchProviderException::class);
         $iterator->next();
     }
 
     /**
-     * @throws \App\Exceptions\UndefinedMailProvider
      */
     public function test_has_next_of_iterator_with_one_provider()
     {
-        $providers = [MailProviderFactory::create(MailProviderFactory::SENDGRID)];
-        $iterator = new MailProviderIterator($providers);
+        /** @var SendGridMailProvider $mockProvider */
+        $mockProvider = $this->mock(SendGridMailProvider::class);
+        $iterator = new MailProviderIterator($mockProvider);
 
         $this->assertTrue($iterator->hasNext());
     }
 
     /**
      * @throws \App\Exceptions\NoSuchProviderException
-     * @throws \App\Exceptions\UndefinedMailProvider
      */
     public function test_next_of_iterator_with_one_provider()
     {
-        $providers = [MailProviderFactory::create(MailProviderFactory::SENDGRID)];
-        $iterator = new MailProviderIterator($providers);
+        /** @var SendGridMailProvider $mockProvider */
+        $mockProvider = $this->mock(
+            SendGridMailProvider::class,
+            function (MockInterface $mock) {
+                $mock->shouldReceive('getName')->once()
+                     ->andReturn('sendGrid');
+            }
+        );
 
+        $iterator = new MailProviderIterator($mockProvider);
         $provider = $iterator->next();
 
-        $this->assertEquals($providers[0]->getName(), $provider->getName());
+        $this->assertEquals('sendGrid', $provider->getName());
     }
 
 
     /**
      * @throws \App\Exceptions\NoSuchProviderException
-     * @throws \App\Exceptions\UndefinedMailProvider
      */
     public function test_2_next_of_iterator_with_one_provider()
     {
-        $providers = [MailProviderFactory::create(MailProviderFactory::SENDGRID)];
-        $iterator = new MailProviderIterator($providers);
+        /** @var SendGridMailProvider $mockProvider */
+        $mockProvider = $this->mock(SendGridMailProvider::class);
 
+        $iterator = new MailProviderIterator($mockProvider);
         $iterator->next();
         $this->expectException(NoSuchProviderException::class);
         $iterator->next();
@@ -66,12 +72,12 @@ class MailProviderIteratorTest extends TestCase
 
     /**
      * @throws \App\Exceptions\NoSuchProviderException
-     * @throws \App\Exceptions\UndefinedMailProvider
      */
     public function test_has_next_after_next_of_iterator_with_one_provider()
     {
-        $providers = [MailProviderFactory::create(MailProviderFactory::SENDGRID)];
-        $iterator = new MailProviderIterator($providers);
+        /** @var SendGridMailProvider $mockProvider */
+        $mockProvider = $this->mock(SendGridMailProvider::class);
+        $iterator = new MailProviderIterator($mockProvider);
 
         $iterator->next();
         $this->assertFalse($iterator->hasNext());
@@ -83,14 +89,21 @@ class MailProviderIteratorTest extends TestCase
      */
     public function test_2_next_of_iterator_with_two_provider()
     {
-        $providers = [
-            MailProviderFactory::create(MailProviderFactory::SENDGRID),
-            MailProviderFactory::create(MailProviderFactory::MAILJET),
-        ];
-        $iterator = new MailProviderIterator($providers);
+        /** @var SendGridMailProvider $mockSendGridProvider */
+        $mockSendGridProvider = $this->mock(SendGridMailProvider::class);
+
+        /** @var SendGridMailProvider $mockMailjetProvider */
+        $mockMailjetProvider = $this->mock(
+            SendGridMailProvider::class,
+            function (MockInterface $mock) {
+                $mock->shouldReceive('getName')->once()
+                     ->andReturn('mailjet');
+            }
+        );
+        $iterator = new MailProviderIterator($mockSendGridProvider, $mockMailjetProvider);
         $iterator->next();
         $provider = $iterator->next();
-        $this->assertEquals($providers[1]->getName(), $provider->getName());
+        $this->assertEquals('mailjet', $provider->getName());
     }
 
 }
